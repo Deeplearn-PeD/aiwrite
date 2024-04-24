e42cf7382d4723da4a5cc0b75cdc93bb39c1ccaf
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-24 15:08:26 -0300

    Added appbar

--- libbygui/main.py
+++ libbygui/main.py
@@ -1,11 +1,38 @@
 import flet as ft
 from libbydbot.brain import LibbyDBot
 
-
+def build_appbar():
+    appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.LIGHT),
+        leading_width=40,
+        title=ft.Text("Libby"),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+            ft.Dropdown(
+                width=150,
+                label="Model",
+                options=[
+                    ft.dropdown.Option("ChatGPT"),
+                    ft.dropdown.Option("Gemma"),
+                    ft.dropdown.Option("Lamma"),
+                ]),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(text="Item 1"),
+                    ft.PopupMenuItem(),  # divider
+                ]
+            ),
+        ],
+    )
+    return appbar
 
 def main(page: ft.Page):
     page.title = "Libby D. Bot"
     page.scroll = "adaptive"
+
+    page.appbar = build_appbar()
     def ask_libby(e):
         if not question.value:
             question.error_text = "Please enter a question"
