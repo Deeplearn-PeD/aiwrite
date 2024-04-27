dee3c0b759801ff97f59c6bb622010758a880d89
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-27 14:34:18 -0300

    Added manuscrpit pages and routing between pages

--- libbygui/main.py
+++ libbygui/main.py
@@ -28,12 +28,14 @@ def build_appbar(page):
     return appbar
 
 def build_navigation_bar(page):
-    page.navigation_bar = ft.NavigationBar(
+    navigation_bar = ft.NavigationBar(
         destinations=[
-            ft.NavigationDestination(icon=ft.icons.DOCUMENT_SCANNER_OUTLINED, label="My Manuscripts"),
+            ft.NavigationDestination(icon=ft.icons.DOCUMENT_SCANNER_OUTLINED, label="Manuscripts"),
             ft.NavigationDestination(icon=ft.icons.EDIT_DOCUMENT, label="Edit"),
-        ]
+        ],
+        on_change=lambda e: page.go('/'+e.control.destinations[e.control.selected_index].label.lower().replace(" ", "_"))
     )
+    return navigation_bar
 def build_response_card():
     card = ft.Card(
         content=ft.Container(
@@ -56,13 +58,70 @@ def build_response_card():
     )
     return card
 
+def build_manuscript_list(page):
+    mlist = ft.Card(
+        content=ft.Container(
+            content=ft.Column(
+                [
+
+                ]
+            ),
+            # width=400,
+            padding=ft.padding.symmetric(vertical=10),
+        )
+    )
+    for man in WKF.get_man_list(10):
+        mlist.content.content.controls.append(
+            ft.ListTile(
+                leading=ft.Icon(ft.icons.FILE_OPEN),
+                title=ft.Text(f'{man.id}. {man.title}'),
+                on_click=lambda e: load_manuscript(page, man.id)
+            )
+        )
+    return mlist
+
+def load_manuscript(page, manid):
+    page.client_storage.set("manid", manid)
+    page.go('/edit')
 def main(page: ft.Page):
     page.title = "Libby D. Bot"
     page.scroll = "adaptive"
     page.client_storage.set("model", "llama")
-
     page.appbar = build_appbar(page)
-    build_navigation_bar(page)
+    nav_bar = build_navigation_bar(page)
+    page.update()
+    def route_change(route):
+        print(route)
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/edit",
+                [
+                    page.appbar,
+                    context,
+                    write_button,
+                    response_card,
+                    nav_bar
+                ],
+            )
+        )
+        if page.client_storage.contains_key("manid"):
+            manid = page.client_storage.get("manid")
+            # response_card.content.content.controls[0].value = WKF.get_manuscript_text(manid)
+            # response_card.update()
+        if page.route == "/manuscripts":
+            page.views.append(
+                ft.View(
+                    "/manuscripts",
+                    [
+                        page.appbar,
+                        build_manuscript_list(page),
+                        nav_bar
+                    ]
+                )
+            )
+        page.update()
+
     def write_man(e):
         if not context.value:
             context.error_text = "Please enter the initial concept of your manuscript."
@@ -72,7 +131,8 @@ def main(page: ft.Page):
             page.add(ft.ProgressRing(), ft.Text("This may take a while..."))
             # page.update()
             page.client_storage.set("context", context.value)
-            WKF = Workflow(model=page.client_storage.get("model"))
+            WKF.set_model(page.client_storage.get("model"))
+
             man = WKF.setup_manuscript(context.value)
             response_card.content.content.controls[0].value = man.title + "\n\n" + man.abstract
             response_card.update()
@@ -80,10 +140,14 @@ def main(page: ft.Page):
 
 
     context = ft.TextField(label="Manuscript concept", multiline=True, min_lines=4)
-
+    write_button = ft.ElevatedButton("Write", on_click=write_man)
     response_card = build_response_card()
-    page.add(context, ft.ElevatedButton("Write", on_click=write_man), response_card)
+    # page.add(context, write_button, response_card)
+    page.on_route_change = route_change
+    page.route = "/edit"
+    page.go(page.route)
 
+WKF = Workflow()
 def run():
     ft.app(main)
 
--- libbygui/workflow.py
+++ libbygui/workflow.py
@@ -1,5 +1,5 @@
 from typing import List, Dict, Any, Optional
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 from libbydbot.brain import LibbyDBot
 from libbydbot.brain.embed import DocEmbedder
 
@@ -14,6 +14,8 @@ class Manuscript(SQLModel, table=True):
     conclusion: Optional[str] = None
 
 
+
+
 class Workflow:
     def __init__(self, db_url: str = "sqlite:///manuscripts.db", model: str = "llama",
                  knowledge_base: str = "embeddings"):
@@ -23,12 +25,62 @@ class Workflow:
         self.libby = LibbyDBot(model=model)
         self.KB = DocEmbedder(name=knowledge_base)
 
+    def set_model(self, model: str):
+        self.libby = LibbyDBot(model=model)
+    def get_man_list(self, n: object = 10) -> List[Manuscript]:
+        with Session(self.engine) as session:
+            statement = select(Manuscript).limit(n)
+            manuscripts = session.exec(statement).all()
+        return manuscripts
+
+    def get_manuscript_text(self, manuscript_id: int) -> Dict[str, Any]:
+        manuscript = self.get_manuscript(manuscript_id)
+        return f"""
+        # {manuscript.title}
+        ## Abstract
+        {manuscript.abstract}
+        ## Introduction
+        {manuscript.introduction}
+        ## Methods
+        {manuscript.methods}
+        ## Discussion
+        {manuscript.discussion}
+        ## Conclusion
+        {manuscript.conclusion}
+        """
+
     def setup_manuscript(self, concept: str):
         title = self.libby.ask(f"Please provide a title for the manuscript, based on this concept: {concept}.\n\n Only return the title, without additional text.")
         knowledge = self.KB.retrieve_docs(concept, num_docs=15)
         self.libby.set_context(self.base_prompt + f"\n\n{concept}" + f"\n\n{knowledge}")
         abstract = self.libby.ask("Please write an abstract for a manuscript, based on the context provided. Only return the abstract text, without additional text.")
         manuscript = Manuscript(title='# ' + title if title is not None else "# title", abstract=abstract)
+        self._save_manuscript(manuscript)
+        return manuscript
+
+    def get_manuscript(self,manuscript_id: int) -> Manuscript:
+        with Session(self.engine) as session:
+            statement = select(Manuscript).where(Manuscript.id == manuscript_id)
+            manuscript = session.exec(statement).first()
+        return manuscript
+
+    def add_section(self, manuscript: Manuscript, section_name: str):
+        section = self.libby.ask(f"Please write the {section_name} section of the manuscript, based on the context provided. Only return the section text, without additional text.")
+        setattr(manuscript, section_name, section)
+        self._save_manuscript(manuscript)
+        return manuscript
+
+    def enhance_section(self, manuscript: Manuscript, section_name: str):
+        section = getattr(manuscript, section_name)
+        enhanced_section = self.libby.ask(f"Please enhance the {section_name} section of the manuscript, based on the context provided. Only return the enhanced section text, without additional text.")
+        setattr(manuscript, section_name, enhanced_section)
+        self._save_manuscript(manuscript)
+        return manuscript
+
+    def criticize_section(self, manuscript: Manuscript, section_name: str):
+        section = getattr(manuscript, section_name)
+        criticized_section = self.libby.ask(f"Please criticize the {section_name} section of the manuscript, based on the context provided. Only return the criticized section text, without additional text.")
+        setattr(manuscript, section_name, criticized_section)
         self._save_manuscript(manuscript)
         return manuscript
 
