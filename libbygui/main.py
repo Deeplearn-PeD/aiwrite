8e1f9eff9bb0cf53a76b77046a652264db1a0ac8
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-12-16 17:07:11 -0300

    Re-enabled the review page

--- libbygui/main.py
+++ libbygui/main.py
@@ -59,7 +59,7 @@ def build_navigation_bar(page):
             ft.NavigationBarDestination(icon=ft.Icons.EDIT_DOCUMENT, label="Edit"),
             ft.NavigationBarDestination(icon=ft.Icons.DOCUMENT_SCANNER_OUTLINED, label="Manuscripts"),
             ft.NavigationBarDestination(icon=ft.Icons.BOOK, label="Knowledge"),# tooltip="Knowledge Base"),
-            ft.NavigationBarDestination(icon=ft.Icons.COFFEE, label="Review", tooltip="Review your text", disabled=True),
+            ft.NavigationBarDestination(icon=ft.Icons.COFFEE, label="Review", tooltip="Review your text", disabled=False),
         ],
         on_change=lambda e: page.go(
             '/' + e.control.destinations[e.control.selected_index].label.lower().replace(" ", "_"))
